ac7ea695fb098a1bcb70e4d45893c9910b41f5cb
Ian Clavel <ianclavel1991@gmail.com>
2025-01-17 01:43:36 +0800

    add: floating nav

--- zxc/app/page.tsx
+++ zxc/app/page.tsx
@@ -1,11 +1,15 @@
 import { FloatingDockDemoMenu } from "@/components/floatdock";
+import { FloatingNavbar } from "@/components/floatingNav";
 
 export default function Home() {
   return (
     <>
       <nav>
-        <FloatingDockDemoMenu />
+        <FloatingNavbar />
       </nav>
+      <aside>
+        <FloatingDockDemoMenu />
+      </aside>
       <main className="w-full h-[100vh] flex items-center justify-center">
         Lorem Ipsum Dolor Sit
       </main>
